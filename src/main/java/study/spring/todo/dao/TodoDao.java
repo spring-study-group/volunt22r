39eea402e8c0929d3dc60429b0fd01dab00ecab3
volunt22r <volunt22r@naver.com>
2015-09-12 16:42:26 +0900

    hibernate 설정

--- src/main/java/study/spring/todo/dao/TodoDao.java
+++ src/main/java/study/spring/todo/dao/TodoDao.java
@@ -1,5 +1,15 @@
 package study.spring.todo.dao;
 
+import org.hibernate.SessionFactory;
+
 public class TodoDao {
+	private SessionFactory sessionFactory;
 	
+	public void setSessionFactory(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public SessionFactory getSessionFactory(){
+		return this.sessionFactory;
+	}
 }
--- src/main/java/study/spring/todo/dao/UserDao.java
+++ src/main/java/study/spring/todo/dao/UserDao.java
@@ -1,5 +1,15 @@
 package study.spring.todo.dao;
 
-public class UserDao {
+import org.hibernate.SessionFactory;
 
+public class UserDao {
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
+
+	public SessionFactory getSessionFactory(){
+		return this.sessionFactory;
+	}
 }
