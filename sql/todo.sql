258b3105c76ec47e32f254208e9e5c2aac45822a
volunt22r <volunt22r@naver.com>
2015-09-12 17:41:12 +0900

    테이블 선언 SQL 수정

--- sql/todo.sql
+++ sql/todo.sql
@@ -2,18 +2,26 @@ create table todo(
 	uid integer auto_increment primary key,
 	title varchar(250),
 	content varchar(1000),
-	repeat tinyint not null,
+	repeat_type tinyint not null,
 	std_day date not null,
 	variable_std_day date not null,
 	insert_timestamp timestamp not null default CURRENT_TIMESTAMP,
-	modified_timestamp timestamp not null default CURRENT_TIMESTAMP
+	modified_timestamp timestamp not null default CURRENT_TIMESTAMP on update current_timestamp
 );
 
-create table user(
+create table todo_user(
 	uid integer auto_increment primary key,
 	mail varchar(100) unique key not null,
 	password varchar(256) not null,
-	join_day date not null default CURRENT_TIMESTAMP,
-	withdraw_day date,
+	join_day datetime default CURRENT_TIMESTAMP,
+	withdraw_day datetime,
 	status enum('L','R','D') not null default 'L'
+);
+
+create table todo_mapper(
+	uid integer auto_increment primary key,
+	mail varchar(100) not null,
+	todo_id integer not null,
+	constraint foreign key (mail) references todo_user (mail) on delete cascade on update cascade,
+	constraint foreign key (todo_id) references todo (uid) on delete cascade on update cascade
 );
