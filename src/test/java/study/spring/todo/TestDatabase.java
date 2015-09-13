85ba9e843629038e5ae3cc187de303b5c6ac04c0
volunt22r <volunt22r@naver.com>
2015-09-13 14:33:09 +0900

    user CRUD 단위테스트를 하나의 단위로 통합

--- src/test/java/study/spring/todo/TestDatabase.java
+++ src/test/java/study/spring/todo/TestDatabase.java
@@ -1,15 +1,30 @@
 package study.spring.todo;
 
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import org.hibernate.PropertyValueException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
+import study.spring.todo.constant.UserStatus;
 import study.spring.todo.model.Todo;
 import study.spring.todo.model.User;
 import study.spring.todo.service.TodoService;
@@ -23,67 +38,76 @@ import study.spring.todo.service.UserService;
 })
 public class TestDatabase {
 	private static String mail = "volunt22r@naver.com";
-	
+	private static String password = "암호화없는비밀번호";
+
+	@Qualifier("testSessionFactory")
 	@Autowired
 	private UserService userService;
 	
 	@Autowired
 	private TodoService todoService;	
 	
+	private Session getSession(){
+		return userService.getUserDao().getSessionFactory().getCurrentSession();
+	}
+	
+	@After
+	public void closeTransactionWhenErrorOccured(){
+		Transaction tx = getSession().getTransaction();
+		if(tx.isActive()){
+			tx.rollback();
+		}
+	}
+	
 	@Test
-	public void testInsertUser(){
+	public void testCRUDUser(){
 		User user = new User();
 		user.setMail(mail);
 		user.setPassword("테스트니깐암호화없이");
 		
-		Session session = userService.getUserDao().getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		session.save(user);
-		session.getTransaction().commit();
-	}
-	
-	@Test
-	public void testUpdateUser(){
-		Session session = userService.getUserDao().getSessionFactory().getCurrentSession();
-		session.beginTransaction();
+		//insert
+		Session session = getSession();
+		Transaction tx = session.beginTransaction();
+		Integer uid = (Integer)session.save(user);
+		Assert.notNull(uid,"insert fail, uid is null");
+		tx.commit();
 		
-		User user = (User)session.createCriteria(User.class).add(Restrictions.eq("mail", mail)).uniqueResult();
+		//select
+		session = getSession();
+		session.beginTransaction();
+		user = (User)session.createCriteria(User.class).add(Restrictions.eq("mail", mail)).uniqueResult();
+		
+		Assert.notNull(user,"select fail, "+mail+" is invalid user");
+		
+		//update
 		user.setPassword("바꾼비밀번호역시암호화없이");
-		
 		session.update(user);
 		session.getTransaction().commit();
 		
-	}
-	
-	@Test 
-	public void testDeleteUser(){
-		Session session = userService.getUserDao().getSessionFactory().getCurrentSession();
+		//delete
+		session = getSession();
 		session.beginTransaction();
-		
-		User user = (User)session.createCriteria(User.class).add(Restrictions.eq("mail", mail)).uniqueResult();
 		session.delete(user);
-		
 		session.getTransaction().commit();
-		
 	}
 	
-	@Test
-	public void testInsertTodo(){
-		
+	@Test(expected=PropertyValueException.class)
+	public void test_User의_mail주소없이_입력테스트(){
+		User user = new User();
+		user.setPassword("테스트비밀번호");
+		Session session = getSession();
+		session.beginTransaction();
+		session.save(user); //need mail
+		session.getTransaction().commit();
 	}
 	
-	@Test
-	public void testUpdateTodo(){
-		
-	}
-	
-	@Test
-	public void testDeleteTodo(){
-		
-	}
-	
-	@Test
-	public void testFindTodo(){
-		
+	@Test(expected=PropertyValueException.class)
+	public void test_User의_password없이_입력테스트(){
+		User user = new User();
+		user.setMail("test@spring.com");
+		Session session = getSession();
+		session.beginTransaction();
+		session.save(user); //need mail
+		session.getTransaction().commit();
 	}
 }
