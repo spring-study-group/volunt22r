f085505b4bb98415d06f1bd27af19fb826611fe2
volunt22r <volunt22r@naver.com>
2015-09-14 17:31:18 +0900

    User의 todo 리스트를 전부 가져오는 로직 구현

--- src/main/java/study/spring/todo/dao/TodoDao.java
+++ src/main/java/study/spring/todo/dao/TodoDao.java
@@ -1,10 +1,15 @@
 package study.spring.todo.dao;
 
+import java.util.Collection;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import study.spring.todo.model.Todo;
+import study.spring.todo.model.User;
 
 public class TodoDao {
 	private SessionFactory sessionFactory;
@@ -37,5 +42,15 @@ public class TodoDao {
 		Transaction tx = session.beginTransaction();
 		session.update(todo);
 		tx.commit();
+	}
+	
+	public Collection<Todo> getTodoList(String userId){
+		Session session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(User.class);
+		criteria = criteria.add(Restrictions.eq("mail", userId));
+		User user = (User)criteria.uniqueResult();
+		System.err.println(user.getPassword());
+		return user.getTodoList();
 	}
 }
