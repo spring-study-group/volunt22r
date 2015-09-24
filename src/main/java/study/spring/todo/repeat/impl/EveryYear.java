e66a913b8008863cfd1a652473f61b59e8341989
volunt22r <volunt22r@naver.com>
2015-09-24 22:12:37 +0900

    매년 반복 strategy 구현 및 단위테스트

--- src/main/java/study/spring/todo/repeat/impl/EveryYear.java
+++ src/main/java/study/spring/todo/repeat/impl/EveryYear.java
@@ -1,17 +1,33 @@
 package study.spring.todo.repeat.impl;
 
 import java.util.Collection;
-import java.util.Date;
+import java.util.HashSet;
 
-import study.spring.todo.parameter.RepeatParam;
+import org.joda.time.DateTime;
+
+import study.spring.todo.model.Todo;
+import study.spring.todo.repeat.RepeatParam;
 import study.spring.todo.repeat.RepeatStrategy;
 
-public class EveryYear implements RepeatStrategy<RepeatParam, Date> {
+public class EveryYear implements RepeatStrategy<RepeatParam, Long> {
 
 	@Override
-	public Collection<Date> getRepeat(RepeatParam param) {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Long> getRepeat(RepeatParam param) {
+		Collection<Long> repeatDays = new HashSet<Long>(1);
+		Todo todo = param.getTodo();
+		DateTime date = todo.getStdDay();
+		DateTime stdDate = param.getStandardDay();
+		
+		//todo가 있는 달이 아니면 패스
+		if(date.getMonthOfYear() != stdDate.getMonthOfYear())
+			return repeatDays;
+		//todo를 만든 년도보다 작을 경우 패스
+		if(date.getYear() > stdDate.getYear())
+			return repeatDays;
+		
+		repeatDays.add(new DateTime(stdDate.getYear(),date.getMonthOfYear(),date.getDayOfMonth(),date.getHourOfDay(),date.getMinuteOfHour()).getMillis());
+		
+		return repeatDays;
 	}
 
 }
--- src/test/java/study/spring/todo/TestRepeat.java
+++ src/test/java/study/spring/todo/TestRepeat.java
@@ -47,7 +47,43 @@ public class TestRepeat {
 		param.setStandardDay(date);
 		result = Repeat.NONE.getStrategy().getRepeat(param);
 		assertThat("result size must be 0",result.size(),is(0));
-		
 	}
 
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testEveryYear(){
+		DateTime date = makeDate("20150503 03:30");
+		DateTime stdDate = makeDate("20150512 10:10");
+		Todo todo = new Todo();
+		todo.setStdDay(stdDate);
+		RepeatParam param = new RepeatParam();
+		param.setStandardDay(date);
+		param.setTodo(todo);
+		
+		Collection<Long> result = Repeat.EVERY_YEAR.getStrategy().getRepeat(param);
+		assertThat("result size must be 1",result.size(),is(1));
+		assertThat("todo date must be "+stdDate.toString(),result,hasItem(stdDate.getMillis()));
+		
+		date = makeDate("20150601 02:20");
+		param.setStandardDay(date);
+		result = Repeat.EVERY_YEAR.getStrategy().getRepeat(param);
+		assertThat("result size must be 0",result.size(),is(0));
+
+		date = makeDate("20150401 02:20");
+		param.setStandardDay(date);
+		result = Repeat.EVERY_YEAR.getStrategy().getRepeat(param);
+		assertThat("result size must be 0",result.size(),is(0));
+
+		date = makeDate("20160501 02:20");
+		param.setStandardDay(date);
+		result = Repeat.EVERY_YEAR.getStrategy().getRepeat(param);
+		assertThat("result size must be 1",result.size(),is(1));
+		assertThat("todo date must be "+stdDate.toString(),result,hasItem(stdDate.plusYears(1).getMillis()));
+
+		date = makeDate("20140501 02:20");
+		param.setStandardDay(date);
+		result = Repeat.EVERY_YEAR.getStrategy().getRepeat(param);
+		assertThat("result size must be 0",result.size(),is(0));
+
+	}
 }
